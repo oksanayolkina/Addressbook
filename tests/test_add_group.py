4a877a420df05e51e7f17d01c2817f686ccd82c3
oksanayolkina <oksana.yolkina@gmail.com>
2017-04-22 19:02:09 +0300

    add assert messages for add and delete group

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -4,6 +4,6 @@ def test_add_group(app):
     test_group = Group(name="gr_name", header="gr_header", footer="gr_footer")
     app.open_group_page()
     app.create_group(test_group)
-    # TODO: Verify message for group creation
+    assert "A new group has been entered into the address book." in app.message()
     app.return_to_group_page()
     # TODO: Verify group in grouplist
--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -2,6 +2,6 @@
 def test_delete_group(app):
     app.open_group_page()
     app.delete_group_by_number(0)
-    # TODO: Verify message
+    assert "Group has been removed" in app.message()
     app.return_to_group_page()
     # TODO: Deletion group in list
