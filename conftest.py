2cef46f4d39bea369a50cd6bad157041a0ff2478
oksanayolkina <oksana.yolkina@gmail.com>
2017-04-22 18:49:10 +0300

    change pytest.fixture(scope=session)

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 from web_api.addressbook_api import AddressBookAPI
 import pytest
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def app():
     app = AddressBookAPI()
     app.login(username="admin", password="secret")
