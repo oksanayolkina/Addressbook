a0a1aa57b0d018d6f51eab6527be6ab79956bae0
oksanayolkina <oksana.yolkina@gmail.com>
2017-04-24 13:17:46 +0300

    add parametrize for add groups

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,9 +1,17 @@
 from models.group import Group
+import pytest
 
-def test_add_group(app, init_login):
-    test_group = Group(name="gr_name", header="gr_header", footer="gr_footer")
+test_data = [
+    Group(name="gr_name", header="gr_header", footer="gr_footer"),
+    Group(name=None, header=None, footer=None),
+    Group(name=11, header=12, footer=13)
+    ]
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(b) for b in test_data])
+def test_add_group(app, init_login, group):
     app.open_group_page()
-    app.create_group(test_group)
+    app.create_group(group)
     assert "A new group has been entered into the address book." in app.message()
     app.return_to_group_page()
     # TODO: Verify group in grouplist
+    
